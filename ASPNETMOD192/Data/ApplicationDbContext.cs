7da720c5a44cf043a48f7be5cd16aa99b91d8497
Diogo da Silva <dhsilva@beyond-vision.pt>
2024-10-24 22:52:26 +0100

    Second Class: Added Staff Class.

--- ASPNETMOD192/Data/ApplicationDbContext.cs
+++ ASPNETMOD192/Data/ApplicationDbContext.cs
@@ -12,5 +12,10 @@ namespace ASPNETMOD192.Data
         }
 
         public DbSet<Client> Clients { get; set; } = default!;
+
+        public DbSet<Staff> Staff {  get; set; } = default!;
     }
+
+
+
 }
---
+++ ASPNETMOD192/Models/Staff.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETMOD192.Models
+{
+    public class Staff
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateOnly Birthday { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string VATNumber { get; set; }
+
+        public DateTime AdmissionDate { get; set; }
+
+        public DateTime DeactivationDate { get; set ;}
+
+        public string? CellPhoneNumber { get; set; }
+
+        [EmailAddress] // TODO Check me
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string StaffNumber { get;set; }
+    }
+}
